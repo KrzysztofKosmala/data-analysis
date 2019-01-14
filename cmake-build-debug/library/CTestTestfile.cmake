a55309bd34722114df4e6dd2e0bdaa541ef27840
Krzysztof <kkosmala9@gmail.com>
2019-01-14 16:56:01 +0100

    remove

--- cmake-build-debug/library/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Krzysztof Kosmala/Desktop/Repozytorium/zadanie1v2/zadanie1/library
-# Build directory: C:/Users/Krzysztof Kosmala/Desktop/Repozytorium/zadanie1v2/zadanie1/cmake-build-debug/library
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
